489fa783d05cedfa4d274358fd289041066dc2f2
Nick <nanthony007@gmail.com>
2020-04-16 12:52:44 -0400

    Adjust databases config.

--- Raiseleaders/settings.py
+++ Raiseleaders/settings.py
@@ -77,15 +77,18 @@ WSGI_APPLICATION = 'Raiseleaders.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE')
+    )
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
