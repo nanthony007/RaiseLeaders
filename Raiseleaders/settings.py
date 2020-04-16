e1077c8f1d0091743aa489a159eb5942f83d05f3
Nick <nanthony007@gmail.com>
2020-04-16 12:17:44 -0400

    Changed to pyhton 3.7.6

--- Raiseleaders/settings.py
+++ Raiseleaders/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'raiseleaders.herokuapp.com']
 
 # Application definition
 
@@ -115,6 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
