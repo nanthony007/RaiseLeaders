fa316526d01d44b2a7f9fffb2da7da696d280be4
Nick Anthony <nanthony007@gmail.com>
2020-06-09 16:31:58 -0400

    Add AttendanceTracker to admin, add user full name and user attendance count to Profile admin.

--- main_site/admin.py
+++ main_site/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from main_site.models import Event, Resource
+from main_site.models import Event, Resource, AttendanceTracker
 
 
 # Register your models here.
@@ -11,5 +11,10 @@ class ResourceAdmin(admin.ModelAdmin):
     list_display = ('category', 'title', 'difficulty', 'belt_level', 'curriculum')
 
 
+class AttendanceTrackerAdmin(admin.ModelAdmin):
+    list_display = ('event', 'attendee', 'is_attending')
+
+
 admin.site.register(Event, EventAdmin)
 admin.site.register(Resource, ResourceAdmin)
+admin.site.register(AttendanceTracker, AttendanceTrackerAdmin)
--- users/admin.py
+++ users/admin.py
@@ -3,7 +3,18 @@ from .models import Profile
 
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'belt', 'joined')
+    list_display = ('get_user_name', 'get_attendance', 'user', 'belt', 'joined')
+
+
+    def get_user_name(self, obj):
+        return f"{obj.user.first_name} {obj.user.last_name}"
+    get_user_name.short_description = 'User Full Name'
+    get_user_name.admin_order_field = '-user__first_name'
+
+
+    def get_attendance(self, obj):
+        return obj.user.attending.count()
+    get_attendance.short_description = 'User Attendance Count'
 
 
 admin.site.register(Profile, ProfileAdmin)
