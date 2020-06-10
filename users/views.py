65a6e76b4bdcd4b412ef6f2119e1aebf4d449231
Nick Anthony <nanthony007@gmail.com>
2020-06-09 22:54:43 -0400

    Reformat Profile page with attendance tracker progress bar and badge.

--- users/views.py
+++ users/views.py
@@ -8,6 +8,10 @@ from .forms import UserRegistrationForm, ProfileUpdateForm, UserUpdateForm
 from main_site.models import Event, AttendanceTracker
 from django.contrib.auth import authenticate, login, logout
 
+from .models import Profile 
+
+import datetime
+
 
 def custom_login(request):
     if request.method == 'POST':
@@ -65,44 +69,88 @@ def profile(request):
                 is_attending=True,
             )
             attend.save()
-
-        u_form = UserUpdateForm(request.POST or None, instance=request.user)
-        p_form = ProfileUpdateForm(
-            request.POST or None, request.FILES, instance=request.user.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(request, f'Your account has been updated!')
+            messages.success(request, f"You've successfully attended class!")
             return redirect('profile')
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
 
+    delta = datetime.date.today() - request.user.profile.joined
+    if request.user.profile.belt.startswith('Red'):
+        belt_progress = 'red'
+    else:  # lower belts
+        belt = request.user.profile.belt
+        required_classes = 24
+        if belt == 'White':
+            required_classes *= 1
+        elif belt == 'White/Yellow':
+            required_classes *= 2
+        elif belt == 'White/Black':
+            required_classes *= 3
+        elif belt == 'Yellow':
+            required_classes *= 4
+        elif belt == 'Yellow/Black':
+            required_classes *= 5
+        elif belt == 'Orange':
+            required_classes *= 6
+        elif belt == 'Orange/Black':
+            required_classes *= 7
+        elif belt == 'Green':
+            required_classes *= 8
+        elif belt == 'Green/Black':
+            required_classes *= 9
+        elif belt == 'Purple':
+            required_classes *= 10
+        elif belt == 'Purple/Black':
+            required_classes *= 11
+        elif belt == 'Blue':
+            required_classes *= 12
+        elif belt == 'Blue/Black':
+            required_classes *= 13
+        elif belt == 'Brown':
+            required_classes *= 14
+        elif belt == 'Brown/Black':
+            required_classes *= 15
+        belt_progress = (request.user.attending.count() / required_classes) * 100
     context = {
         'u_form': u_form,
         'p_form': p_form,
+        'account_days': delta.days,
+        'belt_progress': belt_progress,
     }
     return render(request, 'users/profile.html', context)
 
 
 @login_required
 def profile_update(request):
+    print(request.user.email)
     if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST or None, instance=request.user)
-        p_form = ProfileUpdateForm(
-            request.POST or None, request.FILES, instance=request.user.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(request, r'Your account has been updated!')
-            return redirect('profile')
-
-    else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
-
+        posted_email = request.POST['email']
+        posted_avatar = request.POST['avatar']
+        if request.user.email != posted_email:
+            user = get_object_or_404(User, username=request.user.username)
+            user.email = posted_email
+            user.save()
+            messages.success(request, f"Your email has been updated!")
+        if request.user.profile.image != posted_avatar:
+            profile = get_object_or_404(Profile, user=request.user)
+            profile.image = posted_avatar
+            profile.save()
+            messages.success(request, f"Your avatar has been updated!")
+        # u_form = UserUpdateForm(request.POST or None, instance=request.user)
+        # p_form = ProfileUpdateForm(
+        #     request.POST or None, request.FILES, instance=request.user.profile)
+        #if u_form.is_valid() and p_form.is_valid():
+            # u_form.save()
+            # p_form.save()
+        return redirect('profile')
+    else:  # get
+        pass
+        #u_form = UserUpdateForm(instance=request.user)
+        #p_form = ProfileUpdateForm(instance=request.user.profile)
     context = {
-        'u_form': u_form,
-        'p_form': p_form,
+        #'user_email': user_email,
+        #'u_form': u_form,
+        #'p_form': p_form,
     }
     return render(request, 'users/profile_update.html', context)
