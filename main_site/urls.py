451398b6e66c23e345001b52402798d79638f2f0
Nick Anthony <nanthony007@gmail.com>
2020-06-14 19:04:33 -0400

    New curriculm view based on database listview.

--- main_site/urls.py
+++ main_site/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('curriculum/', views.curriculum, name='curriculum'),
+    path('curriculum/', views.CurriculumnListView.as_view(), name='curriculum'),
     path('about/', views.about, name='about'),
     path('calendar/', views.calendar, name='calendar'),
     path('library/', views.library, name='library'),
--- main_site/views.py
+++ main_site/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.views.generic.list import ListView
+
+from .models import Resource
 
 
 # Create your views here.
@@ -11,11 +14,22 @@ def about(request):
     return render(request, 'main_site/about.html')
 
 
-def curriculum(request):
-    if not request.user.is_authenticated:
-        messages.error(request, f"You need to be logged in to see this page.  Please login or register.")
-        return redirect('login')
-    return render(request, 'main_site/curriculum.html')
+class CurriculumnListView(ListView):
+    model = Resource
+    template_name = 'main_site/curriculum.html'
+    
+    def get_queryset(self):
+        qs = Resource.objects.filter(belt_level=self.request.user.profile.belt).order_by('title')
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if not self.request.user.is_authenticated:
+            messages.error(self.request, f"You need to be logged in to see this page.  Please login or register.")
+            return redirect('login')
+        return context
+
+
 
 
 def library(request):
