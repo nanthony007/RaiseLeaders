ebed715744f031181661a5fa4048ef7088fb8e1a
Nick Anthony <nanthony007@gmail.com>
2020-09-20 16:57:08 -0400

    Add ninja board functionality.

--- main_site/urls.py
+++ main_site/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('curriculum/', views.CurriculumnListView.as_view(), name='curriculum'),
-    path('about/', views.about, name='about'),
-    path('calendar/', views.calendar, name='calendar'),
-    path('library/', views.library, name='library'),
-    path('library/<str:category>/', views.LibraryCategory.as_view(), name='category_view'),
+    path("curriculum/", views.CurriculumnListView.as_view(), name="curriculum"),
+    path("about/", views.about, name="about"),
+    path("calendar/", views.calendar, name="calendar"),
+    path("library/", views.library, name="library"),
+    path(
+        "library/<str:category>/", views.LibraryCategory.as_view(), name="category_view"
+    ),
+    path("ninja-board/", views.ninja_board, name="ninja-board"),
 ]
--- main_site/views.py
+++ main_site/views.py
@@ -7,56 +7,80 @@ from .models import Resource
 
 # Create your views here.
 
+
 def about(request):
     if not request.user.is_authenticated:
-        messages.error(request, f"You need to be logged in to see this page.  Please login or register.")
-        return redirect('login')
-    return render(request, 'main_site/about.html')
+        messages.error(
+            request,
+            f"You need to be logged in to see this page.  Please login or register.",
+        )
+        return redirect("login")
+    return render(request, "main_site/about.html")
 
 
 class CurriculumnListView(ListView):
     model = Resource
-    template_name = 'main_site/curriculum.html'
-    
+    template_name = "main_site/curriculum.html"
+
     def get_queryset(self):
-        qs = Resource.objects.filter(curriculum=True).filter(belt_level=self.request.user.profile.belt).order_by('title')
+        qs = (
+            Resource.objects.filter(curriculum=True)
+            .filter(belt_level=self.request.user.profile.belt)
+            .order_by("title")
+        )
         return qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         if not self.request.user.is_authenticated:
-            messages.error(self.request, f"You need to be logged in to see this page.  Please login or register.")
-            return redirect('login')
+            messages.error(
+                self.request,
+                f"You need to be logged in to see this page.  Please login or register.",
+            )
+            return redirect("login")
         return context
 
 
 def library(request):
     if not request.user.is_authenticated:
-        messages.error(request, f"You need to be logged in to see this page.  Please login or register.")
-        return redirect('login')
-    return render(request, 'main_site/library.html')
+        messages.error(
+            request,
+            f"You need to be logged in to see this page.  Please login or register.",
+        )
+        return redirect("login")
+    return render(request, "main_site/library.html")
 
 
 class LibraryCategory(ListView):
-    model = Resource 
-    template_name = 'main_site/library_category.html'
+    model = Resource
+    template_name = "main_site/library_category.html"
 
     def get_queryset(self, **kwargs):
-        cat = self.kwargs.get('category')
-        if cat == 'poomsae' or cat == 'workouts':
-            qs = Resource.objects.filter(curriculum=False).filter(category=cat.capitalize())
+        cat = self.kwargs.get("category")
+        if cat == "poomsae" or cat == "workouts":
+            qs = Resource.objects.filter(curriculum=False).filter(
+                category=cat.capitalize()
+            )
             return qs
         else:
-            qs = Resource.objects.filter(category=cat.capitalize()).order_by('title')
+            qs = Resource.objects.filter(category=cat.capitalize()).order_by("title")
             return qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['page_name'] = self.kwargs.get('category').capitalize()
+        context["page_name"] = self.kwargs.get("category").capitalize()
         return context
+
 
 def calendar(request):
     if not request.user.is_authenticated:
-        messages.error(request, f"You need to be logged in to see this page.  Please login or register.")
-        return redirect('login')
-    return render(request, 'main_site/calendar.html')
+        messages.error(
+            request,
+            f"You need to be logged in to see this page.  Please login or register.",
+        )
+        return redirect("login")
+    return render(request, "main_site/calendar.html")
+
+
+def ninja_board(request):
+    return render(request, template_name="ninjaboard.html")
